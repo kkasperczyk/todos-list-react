f1dd5756751c8156b8dd528aca48bf386d6ad61d
Konrad Kasperczyk <kkasperczyk20@gmail.com>
2020-07-16 18:01:32 +0200

    Updates

---
+++ precache-manifest.506ca851f5b57fb8794dd7b7cae9df74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ffc5b197234278df012ec09aab8d60",
+    "url": "/todos-list-react/index.html"
+  },
+  {
+    "revision": "946f6d7c9df555a811f6",
+    "url": "/todos-list-react/static/css/main.a5637a88.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946f6d7c9df555a811f6",
+    "url": "/todos-list-react/static/js/main.2a832917.chunk.js"
+  },
+  {
+    "revision": "97d8fa25e5bde6a122ac",
+    "url": "/todos-list-react/static/js/runtime-main.bb6efc59.js"
+  }
+]);
