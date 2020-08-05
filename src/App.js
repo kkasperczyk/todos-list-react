5cd9f3505f159ad78348254cf1fb296503822e00
Konrad Kasperczyk <kkasperczyk20@gmail.com>
2020-08-05 22:26:24 +0200

    changed local storage functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -11,10 +11,12 @@ import Container from "./Container";
 function App() {
   const [hideDone, setHideDone] = useState(false);
   const [tasks, setTasks] = useState(
-    JSON.parse(!localStorage.getItem("tasks"))
-      ? JSON.parse(localStorage.getItem("tasks"))
-      : []
+    JSON.parse(localStorage.getItem("tasks")) || []
   );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone((hideDone) => !hideDone);
