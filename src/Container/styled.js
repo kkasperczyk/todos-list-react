2215a42dda7220b833d3b1cda174dc34b847ad08
Konrad Kasperczyk <kkasperczyk20@gmail.com>
2020-08-15 13:48:06 +0200

    styled components implemented in Container component

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,8 +1,10 @@
-.main {
+import styled from "styled-components";
+
+export const Main = styled.div`
   margin: 10vh auto;
   width: 100%;
   max-width: 1000px;
 
   display: flex;
   flex-flow: column wrap;
-}
+`;
