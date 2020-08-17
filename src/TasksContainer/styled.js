8d97eba823c940f05542326539ac84b5eceec16b
Konrad Kasperczyk <kkasperczyk20@gmail.com>
2020-08-17 19:44:26 +0200

    padding in tasks section title fixed

--- src/TasksContainer/styled.js
+++ src/TasksContainer/styled.js
@@ -4,6 +4,7 @@ export const StyledTasksContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr auto;
   border-bottom: 2px solid #e8e8e8;
+  padding: 20px;
 
   @media (max-width: 767px) {
     grid-template-columns: 1fr;
