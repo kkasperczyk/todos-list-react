89d8249c01cd9443f8812980ad8597e16bfb07b4
Konrad Kasperczyk <kkasperczyk20@gmail.com>
2020-09-27 10:29:18 +0200

    persisting tasks in local storage + example tasks button added

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,26 +1,39 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
+import {
+  selectTasks,
+  toggleHideDone,
+  setAllDone,
+  fetchExampleTasks,
+  selectHideDone,
+} from "../tasksSlice";
 import { ButtonContainer, Button } from "./styled";
 
 const Buttons = () => {
-  const { tasks, hideDone } = useSelector(selectTasks);
+  const tasks = useSelector(selectTasks);
+  const hideDone = useSelector(selectHideDone);
+  
   const dispatch = useDispatch();
 
   return (
-    tasks.length > 0 && (
-      <ButtonContainer>
-        <Button onClick={() => dispatch(toggleHideDone())}>
-          {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
-        </Button>
-        <Button
-          onClick={() => dispatch(setAllDone())}
-          disabled={tasks.every(({ done }) => done)}
-        >
-          Ukończ wszystkie
-        </Button>
-      </ButtonContainer>
-    )
+    <>
+      <Button onClick={() => dispatch(fetchExampleTasks())}>
+        Pobierz przykładowe zadania
+      </Button>
+      {tasks.length > 0 && (
+        <ButtonContainer>
+          <Button onClick={() => dispatch(toggleHideDone())}>
+            {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
+          </Button>
+          <Button
+            onClick={() => dispatch(setAllDone())}
+            disabled={tasks.every(({ done }) => done)}
+          >
+            Ukończ wszystkie
+          </Button>
+        </ButtonContainer>
+      )}
+    </>
   );
 };
 
---
+++ src/features/tasks/tasksLocalStorage.js
@@ -0,0 +1,10 @@
+const localStorageKey = "tasks";
+
+export const saveTasksInLocalStorage = (tasks) =>
+  localStorage.setItem(localStorageKey, JSON.stringify(tasks));
+
+export const getTasksFromLocalStorage = () => {
+  const tasks = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+
+  return tasks;
+};
